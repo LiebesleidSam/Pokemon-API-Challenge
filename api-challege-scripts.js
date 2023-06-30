97a9fb686e31755840ebc27f31ee1509e203c739
Sam Ruiz <samuel.ruiz.mtg@gmail.com>
2023-06-30 10:38:13 -0400

    First Working Version

--- api-challege-scripts.js
+++ api-challege-scripts.js
@@ -1,14 +1,27 @@
 const url = "https://pokeapi.co/api/v2/";
 
-function run () {
-    fetch(url + "generation/1/")
+function updateList (gen, pokemonNumber) {
+    fetch(url + `generation/${gen}`)
         .then(function (response) {
             return response.json();
         })
         .then(function (json) {
-            let listItem = document.createElement("li");
-            listItem.innerText = json.pokemon_species[0].name;
-            document.getElementById("pokemon-list").appendChild(listItem);
+            let listItem = document.createElement("h3");
+            let pokemonName = json.pokemon_species[pokemonNumber].name;
+            listItem.innerText = pokemonName;
+            document.getElementById("pokemon").appendChild(listItem);
         })
     }
 
+function getPokemon () {
+    let generation = document.getElementById("generation").value;
+    let pokemonNumber = document.getElementById("pokemon-number").value;
+    let parentDiv = document.getElementById("pokemon");
+
+    parentDiv.innerHTML = "";
+    
+    updateList(generation, pokemonNumber);
+}
+
+let button = document.getElementById("update");
+button.addEventListener("click", getPokemon);
